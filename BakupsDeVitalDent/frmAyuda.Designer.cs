cb15e19b7140b19beebf233b4715cab19de4d9fa
Yahir <20251168@uthh.edu.mx>
2026-07-09 18:35:57 -0600

    ok

--- BakupsDeVitalDent/frmAyuda.Designer.cs
+++ BakupsDeVitalDent/frmAyuda.Designer.cs
@@ -28,164 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label7 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label8 = new Label();
-            label9 = new Label();
-            label10 = new Label();
-            label11 = new Label();
-            label12 = new Label();
-            label13 = new Label();
+            lblAyuda = new Label();
+            lblPreguntasFrecuentes = new Label();
+            lblPregunta1 = new Label();
+            lblRespuesta1 = new Label();
+            lblPregunta2 = new Label();
+            lblRespuesta2 = new Label();
+            lblPregunta3 = new Label();
+            lblRespuesta3 = new Label();
+            lblpregunta4 = new Label();
+            lblRespuesta4 = new Label();
+            lblPregunta5 = new Label();
+            lblRespuesta5 = new Label();
+            lblContacto = new Label();
             SuspendLayout();
             // 
-            // label1
+            // lblAyuda
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(14, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(100, 38);
-            label1.TabIndex = 0;
-            label1.Text = "Ayuda";
+            lblAyuda.AutoSize = true;
+            lblAyuda.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAyuda.Location = new Point(14, 9);
+            lblAyuda.Name = "lblAyuda";
+            lblAyuda.Size = new Size(100, 38);
+            lblAyuda.TabIndex = 0;
+            lblAyuda.Text = "Ayuda";
             // 
-            // label2
+            // lblPreguntasFrecuentes
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(14, 59);
-            label2.Name = "label2";
-            label2.Size = new Size(240, 31);
-            label2.TabIndex = 1;
-            label2.Text = "Preguntas frecuentes";
+            lblPreguntasFrecuentes.AutoSize = true;
+            lblPreguntasFrecuentes.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPreguntasFrecuentes.Location = new Point(14, 59);
+            lblPreguntasFrecuentes.Name = "lblPreguntasFrecuentes";
+            lblPreguntasFrecuentes.Size = new Size(240, 31);
+            lblPreguntasFrecuentes.TabIndex = 1;
+            lblPreguntasFrecuentes.Text = "Preguntas frecuentes";
             // 
-            // label3
+            // lblPregunta1
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(40, 109);
-            label3.Name = "label3";
-            label3.Size = new Size(255, 20);
-            label3.TabIndex = 2;
-            label3.Text = "¿Cómo registro un nuevo paciente?";
+            lblPregunta1.AutoSize = true;
+            lblPregunta1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPregunta1.Location = new Point(40, 109);
+            lblPregunta1.Name = "lblPregunta1";
+            lblPregunta1.Size = new Size(345, 28);
+            lblPregunta1.TabIndex = 2;
+            lblPregunta1.Text = "¿Cómo registro un nuevo paciente?";
             // 
-            // label7
+            // lblRespuesta1
             // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label7.Location = new Point(42, 135);
-            label7.Name = "label7";
-            label7.Size = new Size(664, 20);
-            label7.TabIndex = 6;
-            label7.Text = "Dirígete al módulo Pacientes, haz clic en Nuevo, completa los datos solicitados y presiona Guardar.";
+            lblRespuesta1.AutoSize = true;
+            lblRespuesta1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRespuesta1.Location = new Point(40, 137);
+            lblRespuesta1.Name = "lblRespuesta1";
+            lblRespuesta1.Size = new Size(664, 20);
+            lblRespuesta1.TabIndex = 6;
+            lblRespuesta1.Text = "Dirígete al módulo Pacientes, haz clic en Nuevo, completa los datos solicitados y presiona Guardar.";
             // 
-            // label4
+            // lblPregunta2
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(42, 184);
-            label4.Name = "label4";
-            label4.Size = new Size(341, 20);
-            label4.TabIndex = 7;
-            label4.Text = "¿Cómo modifico la información de un paciente?";
+            lblPregunta2.AutoSize = true;
+            lblPregunta2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPregunta2.Location = new Point(40, 185);
+            lblPregunta2.Name = "lblPregunta2";
+            lblPregunta2.Size = new Size(464, 28);
+            lblPregunta2.TabIndex = 7;
+            lblPregunta2.Text = "¿Cómo modifico la información de un paciente?";
             // 
-            // label5
+            // lblRespuesta2
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(40, 213);
-            label5.Name = "label5";
-            label5.Size = new Size(642, 20);
-            label5.TabIndex = 8;
-            label5.Text = "Busca al paciente, selecciónalo de la lista, realiza los cambios necesarios y haz clic en Actualizar";
+            lblRespuesta2.AutoSize = true;
+            lblRespuesta2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRespuesta2.Location = new Point(40, 213);
+            lblRespuesta2.Name = "lblRespuesta2";
+            lblRespuesta2.Size = new Size(642, 20);
+            lblRespuesta2.TabIndex = 8;
+            lblRespuesta2.Text = "Busca al paciente, selecciónalo de la lista, realiza los cambios necesarios y haz clic en Actualizar";
             // 
-            // label6
+            // lblPregunta3
             // 
-            label6.AutoSize = true;
-            label6.Location = new Point(42, 252);
-            label6.Name = "label6";
-            label6.Size = new Size(205, 20);
-            label6.TabIndex = 9;
-            label6.Text = "¿Cómo elimino un paciente?";
+            lblPregunta3.AutoSize = true;
+            lblPregunta3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPregunta3.Location = new Point(42, 253);
+            lblPregunta3.Name = "lblPregunta3";
+            lblPregunta3.Size = new Size(278, 28);
+            lblPregunta3.TabIndex = 9;
+            lblPregunta3.Text = "¿Cómo elimino un paciente?";
             // 
-            // label8
+            // lblRespuesta3
             // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label8.Location = new Point(42, 281);
-            label8.Name = "label8";
-            label8.Size = new Size(782, 20);
-            label8.TabIndex = 10;
-            label8.Text = "Selecciona al paciente y presiona el botón Eliminar. El sistema solicitará una confirmación antes de borrar el registro.";
+            lblRespuesta3.AutoSize = true;
+            lblRespuesta3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRespuesta3.Location = new Point(42, 281);
+            lblRespuesta3.Name = "lblRespuesta3";
+            lblRespuesta3.Size = new Size(782, 20);
+            lblRespuesta3.TabIndex = 10;
+            lblRespuesta3.Text = "Selecciona al paciente y presiona el botón Eliminar. El sistema solicitará una confirmación antes de borrar el registro.";
             // 
-            // label9
+            // lblpregunta4
             // 
-            label9.AutoSize = true;
-            label9.Location = new Point(42, 311);
-            label9.Name = "label9";
-            label9.Size = new Size(197, 20);
-            label9.TabIndex = 11;
-            label9.Text = "¿Cómo programo una cita?";
+            lblpregunta4.AutoSize = true;
+            lblpregunta4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblpregunta4.Location = new Point(42, 311);
+            lblpregunta4.Name = "lblpregunta4";
+            lblpregunta4.Size = new Size(265, 28);
+            lblpregunta4.TabIndex = 11;
+            lblpregunta4.Text = "¿Cómo programo una cita?";
             // 
-            // label10
+            // lblRespuesta4
             // 
-            label10.AutoSize = true;
-            label10.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label10.Location = new Point(42, 331);
-            label10.Name = "label10";
-            label10.Size = new Size(910, 20);
-            label10.TabIndex = 12;
-            label10.Text = "En el módulo Agenda de Citas, selecciona el paciente, elige la fecha y hora disponibles, escribe el motivo de la consulta y guarda la cita.";
+            lblRespuesta4.AutoSize = true;
+            lblRespuesta4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRespuesta4.Location = new Point(42, 339);
+            lblRespuesta4.Name = "lblRespuesta4";
+            lblRespuesta4.Size = new Size(910, 20);
+            lblRespuesta4.TabIndex = 12;
+            lblRespuesta4.Text = "En el módulo Agenda de Citas, selecciona el paciente, elige la fecha y hora disponibles, escribe el motivo de la consulta y guarda la cita.";
             // 
-            // label11
+            // lblPregunta5
             // 
-            label11.AutoSize = true;
-            label11.Location = new Point(42, 367);
-            label11.Name = "label11";
-            label11.Size = new Size(179, 20);
-            label11.TabIndex = 13;
-            label11.Text = "¿Cómo cancelo una cita?";
+            lblPregunta5.AutoSize = true;
+            lblPregunta5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPregunta5.Location = new Point(42, 367);
+            lblPregunta5.Name = "lblPregunta5";
+            lblPregunta5.Size = new Size(244, 28);
+            lblPregunta5.TabIndex = 13;
+            lblPregunta5.Text = "¿Cómo cancelo una cita?";
             // 
-            // label12
+            // lblRespuesta5
             // 
-            label12.AutoSize = true;
-            label12.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label12.Location = new Point(42, 387);
-            label12.Name = "label12";
-            label12.Size = new Size(758, 20);
-            label12.TabIndex = 14;
-            label12.Text = "Busca la cita en la agenda, selecciónala y utiliza la opción Cancelar o Eliminar, según la configuración del sistema";
+            lblRespuesta5.AutoSize = true;
+            lblRespuesta5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblRespuesta5.Location = new Point(42, 395);
+            lblRespuesta5.Name = "lblRespuesta5";
+            lblRespuesta5.Size = new Size(758, 20);
+            lblRespuesta5.TabIndex = 14;
+            lblRespuesta5.Text = "Busca la cita en la agenda, selecciónala y utiliza la opción Cancelar o Eliminar, según la configuración del sistema";
             // 
-            // label13
+            // lblContacto
             // 
-            label13.AutoSize = true;
-            label13.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label13.Location = new Point(42, 442);
-            label13.Name = "label13";
-            label13.Size = new Size(408, 100);
-            label13.TabIndex = 15;
-            label13.Text = "¿Necesitas otra ayuda?\r\n\r\nCorreo: soporte@vitaldent.com\r\nTeléfono: (000) 000-0000\r\nHorario de atención: Lunes a Viernes de 9:00 a.m. a 6:00 p.m.";
+            lblContacto.AutoSize = true;
+            lblContacto.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblContacto.Location = new Point(42, 442);
+            lblContacto.Name = "lblContacto";
+            lblContacto.Size = new Size(408, 100);
+            lblContacto.TabIndex = 15;
+            lblContacto.Text = "¿Necesitas otra ayuda?\r\n\r\nCorreo: soporte@vitaldent.com\r\nTeléfono: (000) 000-0000\r\nHorario de atención: Lunes a Viernes de 9:00 a.m. a 6:00 p.m.";
             // 
             // frmAyuda
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1479, 627);
-            Controls.Add(label13);
-            Controls.Add(label12);
-            Controls.Add(label11);
-            Controls.Add(label10);
-            Controls.Add(label9);
-            Controls.Add(label8);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label7);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(lblContacto);
+            Controls.Add(lblRespuesta5);
+            Controls.Add(lblPregunta5);
+            Controls.Add(lblRespuesta4);
+            Controls.Add(lblpregunta4);
+            Controls.Add(lblRespuesta3);
+            Controls.Add(lblPregunta3);
+            Controls.Add(lblRespuesta2);
+            Controls.Add(lblPregunta2);
+            Controls.Add(lblRespuesta1);
+            Controls.Add(lblPregunta1);
+            Controls.Add(lblPreguntasFrecuentes);
+            Controls.Add(lblAyuda);
             Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Name = "frmAyuda";
             Text = "frmAyuda";
@@ -195,18 +200,18 @@
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label7;
-        private Label label4;
-        private Label label5;
-        private Label label6;
-        private Label label8;
-        private Label label9;
-        private Label label10;
-        private Label label11;
-        private Label label12;
-        private Label label13;
+        private Label lblAyuda;
+        private Label lblPreguntasFrecuentes;
+        private Label lblPregunta1;
+        private Label lblRespuesta1;
+        private Label lblPregunta2;
+        private Label lblRespuesta2;
+        private Label lblPregunta3;
+        private Label lblRespuesta3;
+        private Label lblpregunta4;
+        private Label lblRespuesta4;
+        private Label lblPregunta5;
+        private Label lblRespuesta5;
+        private Label lblContacto;
     }
 }
